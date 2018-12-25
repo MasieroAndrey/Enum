e3fb9cae10ec4ec2f08316b42712641a8147f3c7
Andrey Masiero <andrey.masiero@hotmail.com>
2018-12-24 22:50:30 -0200

    Classe order completa

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,71 @@
 package entities;
 
-public class Order {
+import java.util.Date;
 
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Integer id;
+	private Date moment;
+	private OrderStatus status;
+	
+	public Order() {}
+
+	/**
+	 * @param id
+	 * @param moment
+	 * @param status
+	 */
+	public Order(Integer id, Date moment, OrderStatus status) {
+		
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the moment
+	 */
+	public Date getMoment() {
+		return moment;
+	}
+
+	/**
+	 * @param moment the moment to set
+	 */
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	
+
+	
 }
